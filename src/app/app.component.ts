202883d363cb26a5a73941fc30c65ee964b46817
italosantana <flaubert165@gmail.com>
2017-11-20 10:13:37 -0300

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private _httpService: Http) { }
+  apiValues: string[] = [];
+  ngOnInit() {
+     this._httpService.get('http://localhost:5000/api/values').subscribe(values => {
+        this.apiValues = values.json() as string[];
+     });
+  }
+}
